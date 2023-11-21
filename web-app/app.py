0df9de065e12b7c0ba16a618a73f59bfbf048942
Juliann Zhou <kyz224@nyu.edu>
2023-11-21 16:55:02 -0500

    test reformat

--- web-app/app.py
+++ web-app/app.py
@@ -14,36 +14,41 @@ load_dotenv()
 app = Flask(__name__)
 
 # Connecting to local host
-client = pymongo.MongoClient(os.getenv('MONGO_URI'))
-db = client[os.getenv('MONGO_DBNAME')]
+client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("MONGO_DBNAME")]
 images = db.images
 fs = gridfs.GridFS(db)
 
 
-
-
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     """
     Home Route - Display the upload form
     """
     return render_template("capture.html")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
     """
     Handle image upload
     """
-    if 'image' in request.files:
-        image = request.files['image']
+    if "image" in request.files:
+        image = request.files["image"]
         content_type = image.content_type
         image_id = fs.put(image, content_type=content_type, filename=image.filename)
-        images.insert_one({"filename": image.filename,
-                           "contentType": content_type, "gridfs_id": image_id})
-        return redirect(url_for('index'))
+        images.insert_one(
+            {
+                "filename": image.filename,
+                "contentType": content_type,
+                "gridfs_id": image_id,
+            }
+        )
+        return redirect(url_for("index"))
     return render_template("capture.html")
 
-@app.route('/trigger-ml-processing', methods=['GET'])
+
+@app.route("/trigger-ml-processing", methods=["GET"])
 def trigger_ml_processing():
     """
     api endpoint
@@ -56,4 +61,4 @@ def trigger_ml_processing():
 
 if __name__ == "__main__":
     PORT = 5000
-    app.run(debug=True, host='0.0.0.0', port=PORT)
+    app.run(debug=True, host="0.0.0.0", port=PORT)
